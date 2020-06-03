953216ffd439398d9fcb79760b2be7717f52f632
Anouk <ambrondijk@gmail.com>
2020-06-03 10:23:41 +0200

    hello

---
+++ VercelHelloWorld/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VercelHelloWorld
+{
+    public class Name
+    {
+        public string myName { get; set; }
+
+
+        public Name(string name)
+        {
+            this.myName = name;
+        }
+    }
+
+
+}
